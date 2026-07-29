210d6516f00a7e3b9917697c4e3a87eb999750c9
Hana Mohamed <hanaosmannn121@gmail.com>
2026-07-30 02:08:40 +0300

    Update reviewRoutes.js

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,5 +1,19 @@
-const express = require("express");
 
+const express = require("express");
 const router = express.Router();
+
+const {
+  createReview,
+  getReviews,
+  getReviewById,
+  updateReview,
+  deleteReview,
+} = require("../controllers/reviewController");
+
+router.post("/", createReview);
+router.get("/", getReviews);
+router.get("/:id", getReviewById);
+router.put("/:id", updateReview);
+router.delete("/:id", deleteReview);
 
 module.exports = router;
